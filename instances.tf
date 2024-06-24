1343226dd821629207e4b572429b738ef20e53c6
Reggie Jones <reggiejones@Reggies-MBP.localdomain>
2024-06-24 11:31:50 -0500

    Update instance ip

--- instances.tf
+++ instances.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "master_server" {
   subnet_id              = data.aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.my_public_app_sg.id]
   key_name               = var.my_keypair
-  private_ip             = "192.168.1.50"
+  private_ip             = "192.168.1.51"
 
   tags = {
     Name = "master_node_server"
