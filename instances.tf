bc4220111f11e680eaabcf43fefe1ee17b953fcb
Reggie Jones <reggiejones@Reggies-MBP.localdomain>
2024-06-25 17:48:50 -0500

    Update instance size

--- instances.tf
+++ instances.tf
@@ -6,6 +6,9 @@ resource "aws_instance" "master_server" {
   vpc_security_group_ids = [aws_security_group.my_public_app_sg.id]
   key_name               = var.my_keypair
   private_ip             = "192.168.1.51"
+  root_block_device {
+    volume_size = 100
+  }
 
   tags = {
     Name = "master_node_server"
