110a37ccdbb6115a9ea9de3c942c7c4385e41228
Reggie Jones <regjones16@outlook.com>
2024-06-28 14:10:51 -0500

    test signing

--- security-groups.tf
+++ security-groups.tf
@@ -15,13 +15,13 @@ resource "aws_security_group" "my_public_app_sg" {
   }
 
   // # HTTP Access
-  ingress {
-    description = "Allow HTTP into the EC2"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   description = "Allow HTTP into the EC2"
+  #   from_port   = 80
+  #   to_port     = 80
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   // # HTTP Access
   // ingress {
